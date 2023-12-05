d7fa92f65b48c7a9c2739c4ad29e7b826d941fcd
Nabeel <42202507+NabeelFarooq@users.noreply.github.com>
2023-12-05 19:18:56 +0530

    Design a 16*16 square div sketch pad

--- javascript.js
+++ javascript.js
@@ -1,38 +1,58 @@
 const container = document.querySelector(".container");
 
 //row 0
-const squareDiv00 = document.createElement("div");
-container.appendChild(squareDiv00);
+// const squareDiv00 = document.createElement("div");
+// container.appendChild(squareDiv00);
 
-const squareDiv01 = document.createElement("div");
-container.appendChild(squareDiv01);
+// const squareDiv01 = document.createElement("div");
+// container.appendChild(squareDiv01);
 
-const squareDiv10 = document.createElement("div");
-container.appendChild(squareDiv10);
+// const squareDiv10 = document.createElement("div");
+// container.appendChild(squareDiv10);
 
-const squareDiv11 = document.createElement("div");
-container.appendChild(squareDiv11);
+// const squareDiv11 = document.createElement("div");
+// container.appendChild(squareDiv11);
 
-// array for row 1
-var squareDiv1 = [];
-for(let i=0;i<4;++i){
-    squareDiv1[i] = document.createElement("div");
-    container.appendChild(squareDiv1[i]);
+// // array for row 1
+// var squareDiv1 = [];
+// for(let i=0;i<4;++i){
+//     squareDiv1[i] = document.createElement("div");
+//     container.appendChild(squareDiv1[i]);
+// }
+// // array for row 2
+// var squareDiv2 = [];
+// for(let i=0;i<4;++i){
+//     squareDiv2[i] = document.createElement("div");
+//     container.appendChild(squareDiv2[i]);
+// }
+// // array for row 3
+// var squareDiv3 = [];
+// for(let i=0;i<4;++i){
+//     squareDiv3[i] = document.createElement("div");
+//     container.appendChild(squareDiv3[i]);
+// }
+
+// code for 8*8 div
+
+// for(let outer=0;outer<8;++outer){
+//     let squareDiv[outer]
+//     for (var i = 0; i < 10; i++) {
+//         this["squareDiv"+outer+i] = "MY VAL";
+//      }
+     
+//     for(let i=0;i<4;++i){
+//         squareDiv3[i] = document.createElement("div");
+//         container.appendChild(squareDiv3[i]);
+//     }
+// }
+for(let outer=0;outer<16;++outer){
+    for(let inner=0;inner<16;++inner){
+        this["squareDiv"+outer+inner] = document.createElement("div");
+        container.appendChild(this["squareDiv"+outer+inner]);
+        this["squareDiv"+outer+inner].textContent = "squareDiv "+outer+inner;
+    }
 }
-// array for row 2
-var squareDiv2 = [];
-for(let i=0;i<4;++i){
-    squareDiv2[i] = document.createElement("div");
-    container.appendChild(squareDiv2[i]);
-}
-// array for row 3
-var squareDiv3 = [];
-for(let i=0;i<4;++i){
-    squareDiv3[i] = document.createElement("div");
-    container.appendChild(squareDiv3[i]);
-}
 
-// code for 8
 // const grid = document.querySelectorAll(".container div");
 
 
